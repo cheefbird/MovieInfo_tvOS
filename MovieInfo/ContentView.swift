4738dd233d898b6a42fa8c8847b5941fe48d21b6
Francis Breidenbach <fbreidenbach@gmail.com>
2020-06-08 20:54:13 -0700

    Initial Commit

---
+++ MovieInfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieInfo
+//
+//  Created by Breidenbach, Francis X. -ND on 6/8/20.
+//  Copyright © 2020 Francis Breidenbach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
